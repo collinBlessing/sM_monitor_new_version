2ca990f33b6594fe4e757ea7778154369055417e
collinBlessing <nahuriracollinblessing@gmail.com>
2023-11-22 16:47:25 +0300

    stablised the app before activating respective menus

--- app/src/main/java/com/example/sm_monitor_new_version/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/sm_monitor_new_version/ui/home/HomeFragment.java
@@ -4,10 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -16,22 +14,17 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.sm_monitor_new_version.R;
 import com.example.sm_monitor_new_version.databinding.FragmentHomeBinding;
 
-import java.util.List;
-
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
-    private HomeViewModel homeViewModel;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.autoCompleteText;
-
         // Get the HomeViewModel
-        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
         // Initialize AutoCompleteTextView
         AutoCompleteTextView autoCompleteTextView = root.findViewById(R.id.auto_complete_text);
@@ -42,12 +35,8 @@ public class HomeFragment extends Fragment {
             autoCompleteTextView.setAdapter(adapter_items);
         });
 
-        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String item = adapterView.getItemAtPosition(i).toString();
-                // Handle item selection as needed
-            }
+        autoCompleteTextView.setOnItemClickListener((adapterView, view, i, l) -> {
+            // Handle item selection as needed
         });
 
         return root;
